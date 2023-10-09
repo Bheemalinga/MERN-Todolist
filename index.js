66b4a5f53598687e3d3e8c54644b9afd162db397
Bheemalinga <kummarabheema@gmail.com>
2023-10-09 14:55:01 +0530

    Snippet code's help to handle CORS issues, when trying to access API from diff domains

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const app = express();
 
 const port = process.env.PORT || 7000;
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
   if (port == 7000) {
