dda5eee224658a40c1392e348d295665a3b343d6
Bheemalinga <kummarabheema@gmail.com>
2023-10-09 15:02:32 +0530

    documentation of middleware function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 7000;
 
+// This snippet of code is used to handle Cross origin Resource Sharing(CORS) error
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
