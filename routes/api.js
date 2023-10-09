7d9209fc7b2e2bd2241b11b8e6ef30511aeb671c
Bheemalinga <kummarabheema@gmail.com>
2023-10-09 15:17:18 +0530

    Skeleton of Express js Router module

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/todos", (req, res, next) => {
+  // get placeholder
+});
+
+router.post("/todos", (req, res, next) => [
+  // Post placeholder
+]);
+
+router.delete("/todos/:id", (req, res, next) => {
+  // Delete placeholder
+});
+
+module.exports = router;
