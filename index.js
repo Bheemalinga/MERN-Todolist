b930cdbf58a35e143429d3300f050ea976877dc5
Bheemalinga <kummarabheema@gmail.com>
2023-10-09 12:17:18 +0530

    Listening the port

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 7000;
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept"
+//   );
+//   next();
+// });
+
+app.listen(port, () => {
+  if (port == 7000) {
+    console.log(`Server is running on http://localhost:${port}`);
+  }
+});
