f93e321c851bd08c15affaec5f16292267ff8547
Bheemalinga <kummarabheema@gmail.com>
2023-10-12 12:26:41 +0530

    Api's working

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+const Todo = require("../models/todo"); // Importing the Todo Model Schema
+
 router.get("/todos", (req, res, next) => {
-  // get placeholder
+  // Returing all the todos, exposing only id and action field.
+  Todo.find({}, "action")
+    .then((data) => res.json(data))
+    .catch(next);
 });
 
-router.post("/todos", (req, res, next) => [
-  // Post placeholder
-]);
+router.post("/todos", (req, res, next) => {
+  if (req.body.action) {
+    Todo.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: "The input field is Empty",
+    });
+  }
+});
 
 router.delete("/todos/:id", (req, res, next) => {
-  // Delete placeholder
+  Todo.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
 });
 
 module.exports = router;
